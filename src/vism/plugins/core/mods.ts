e6f8c5bc4cdeb32ff37ab06a204d69549b1a417b
Iamproplayer7 <iamproplayer7@gmail.com>
2025-12-15 21:48:15 +0100

    fix mods.json path for application

--- src/vism/plugins/core/mods.ts
+++ src/vism/plugins/core/mods.ts
@@ -1,5 +1,14 @@
 import fs from 'fs';
 
+// get current working dir
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 type Pos = { x: number, y: number, z: number };
 
 export type CarInfo = {
@@ -73,7 +82,7 @@ export type CarInfo = {
 let cached: false | CarInfo[] = false;
 export const getModBinData = (id: string) => {
     if(!cached) {
-        cached = JSON.parse(fs.readFileSync('./src/vism/plugins/core/mods.json', 'utf-8')) as CarInfo[];
+        cached = JSON.parse(fs.readFileSync(path.join(__dirname, '/mods.json'), 'utf-8')) as CarInfo[];
     }
 
     if(!cached) return false;
