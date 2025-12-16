8dd4243fa0e814b1b3a39f1bde795b6f68dcabe6
Iamproplayer7 <iamproplayer7@gmail.com>
2025-12-16 21:12:20 +0100

    Auto reconnect if InSim can't connect

--- src/vism/main/classes/Server.ts
+++ src/vism/main/classes/Server.ts
@@ -5,10 +5,14 @@ import { Event } from "./Event.js";
 import { EventType } from "../enums/event.js";
 
 export type Server = {
+    Host: string;
+    Port: number;
+    disconnected: boolean;
     InSimHandle: InSim,
 
     isLocal: boolean,
     connect: (Host: string, Port: number) => void,
+    reconnect: () => void,
     disconnect: () => void,
     message: (text: string, sound?: number) => void,
     command: (text: string) => void
@@ -21,12 +25,25 @@ export const Server = {
         const InSimHandle = new InSim(InSimOptions);
         
         const ServerObject: Server = {
+            Host: '',
+            Port: 0,
+            disconnected: false,
             InSimHandle: InSimHandle,
             isLocal: InSimOptions.Flags ? (InSimOptions.Flags & InSimFlags.ISF_LOCAL) !== 0 : false,
 
             connect(Host, Port) {
+                this.Host = Host;
+                this.Port = Port;
+                this.disconnected = false;
+
                 console.log('[InSim] Connecting to ' + Host + ':' + Port);
                 this.InSimHandle.connect({ Host, Port });
+            },
+
+            reconnect() {
+                this.disconnected = false;
+                console.log('[InSim] Reconnecting to ' + this.Host + ':' + this.Port);
+                this.InSimHandle.connect({ Host: this.Host, Port: this.Port });
             },
 
             disconnect() {
@@ -53,11 +70,21 @@ export const Server = {
         InSimHandle.on('connected', () => {
             console.log('[InSim] Connected');
             Event.fire(EventType.SERVER_CONNECTED, ServerObject);
+
+            ServerObject.disconnected = false;
         });
 
         InSimHandle.on('disconnect', () => {
+            if(ServerObject.disconnected) return;
+            ServerObject.disconnected = true;
+
             console.log('[InSim] Disconnected');
             Event.fire(EventType.SERVER_DISCONNECTED, ServerObject);
+
+            // try reconnect
+            setTimeout(() => {
+                ServerObject.reconnect();
+            }, 2000);
         });
 
         this.all.push(ServerObject);
