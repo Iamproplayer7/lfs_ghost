0b6d544bb4008a2f4a6c80eb43eaa555fa804e07
Iamproplayer7 <iamproplayer7@gmail.com>
2025-12-21 23:58:20 +0100

    version update to 1.0.5

--- src/app/index_app.ts
+++ src/app/index_app.ts
@@ -20,7 +20,7 @@ type TApp = {
 
 const MAIN_PATH = path.join(app.getPath('userData'), '/data');
 
-const VERSION = '1.0.4';
+const VERSION = '1.0.5';
 
 export const AppA: TApp = {
     window: false,
