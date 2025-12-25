911f4a4c5b8352e078a12894ae9cb3e4d5bf73ad
Iamproplayer7 <iamproplayer7@gmail.com>
2025-12-25 19:46:04 +0200

    add coloring functions

--- src/view/utils.js
+++ src/view/utils.js
@@ -15,3 +15,24 @@ const toRad = (deg) => {
 const toDeg = (rad) => {
 	return rad * (180 / Math.PI);
 }
+
+const clamp = (v, min, max) => {
+    return Math.min(Math.max(v, min), max);
+}
+
+const speedToHexColor = (speed, centerSpeed, range = 5) => {
+	let t = (speed - centerSpeed) / range;
+	t = clamp(t, -1, 1);
+
+	let r, g;
+
+	if (t < 0) {
+		r = 255;
+		g = Math.round(255 * (1 + t));
+	} else {
+		r = Math.round(255 * (1 - t));
+		g = 255;
+	}
+
+	return (r << 16) | (g << 8);
+}
